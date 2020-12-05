9be9ef1d016ef9b96b7edd33bbdaa41ef0d66ff4
rosakim1101 <rosakim1101@naver.com>
2020-12-06 01:23:01 +0900

    1206 commit

--- mindtest/urls.py
+++ mindtest/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:mindtest_id>/', views.detail, name='detail'),
+    path('<int:mindtest_id>/results/', views.results, name='results'),
+    path('<int:mindtest_id>/vote/', views.vote, name='vote')
 ]
+
+app_name= 'mindtest'
--- polls/views.py
+++ polls/views.py
@@ -69,15 +69,3 @@ def vote(request, question_id):
 # def vote(request, question_id):
 #     return HttpResponse("You're voting on question %s." % question_id)
 
-def create(requset, requset_id):
- question = get_object_or_404(Question, pk=question_id)
- try:
-     selected_choice = question.choice_set.get(pk=request.POST['choice'])
- except (KeyError, Choice.DoesNotExist):
-     return render(request, 'polls/detail.html', {
-         'question': question,
-         'error_message': "You didn't select a choice."
-     })
-
-def main(request):
-    return render(request, 'polls/index.html')
